8c57157d21fa129d8aed734196b6e18c3887b3da
Ramy Ouda <ramyouda@gmail.com>
2024-12-24 01:46:57 -0500

    add material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
-    provideStore()
+    provideStore(), provideAnimationsAsync()
 ],
 };
