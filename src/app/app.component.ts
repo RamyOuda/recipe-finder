d414fcd6bcc996af0339e1dbd35dd2eccf41b443
Ramy Ouda <ramyouda@gmail.com>
2024-12-25 10:17:50 -0500

    implement dynamic form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,18 @@ import {
   inject,
   OnInit,
 } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+} from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { map, Observable, of, startWith } from 'rxjs';
 import { AppStore } from './store/app.store';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +28,7 @@ import { AppStore } from './store/app.store';
     MatAutocompleteModule,
     ReactiveFormsModule,
     AsyncPipe,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -33,8 +40,19 @@ export class AppComponent implements OnInit {
   readonly formattedItems = this.#store.formattedItems;
   readonly loading = this.#store.loading;
 
-  myControl = new FormControl('');
-  filteredOptions: Observable<string[]> = of([]);
+  equipmentForm = new FormGroup({
+    hat: new FormControl(null),
+    cloak: new FormControl(null),
+    belt: new FormControl(null),
+    boots: new FormControl(null),
+    amulet: new FormControl(null),
+    ring1: new FormControl(null),
+    ring2: new FormControl(null),
+    shield: new FormControl(null),
+    weapon: new FormControl(null),
+  });
+
+  filteredOptions: Observable<any[]> = of([]);
 
   constructor() {
     effect(() => {
@@ -44,21 +62,37 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.#store.fetchData();
+  }
 
-    this.filteredOptions = this.myControl.valueChanges.pipe(
+  onInputSelected(itemType: string) {
+    const formControl = this.equipmentForm.get(itemType) as FormControl;
+
+    this.filteredOptions = formControl.valueChanges.pipe(
       startWith(''),
-      map((value) => this.filter(value || ''))
+      map((value) => this.filter(value ?? '', itemType))
     );
   }
 
-  private filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    const items = this.formattedItems();
+  autocompleteDisplay(item: any): string {
+    return item?.name ?? '';
+  }
 
-    return items
-      ? items.hat
-          .map((hat) => hat.name)
-          .filter((name) => name.toLowerCase().includes(filterValue)) ?? []
-      : [];
+  onSubmit() {
+    console.log(this.equipmentForm.value);
+  }
+
+  private filter(value: string | any, itemType: string): any[] {
+    const items = this.formattedItems();
+    const filterValue = (
+      typeof value === 'string' ? value : value.name
+    ).toLowerCase();
+
+    if (items) {
+      return items[itemType as keyof typeof items].filter((item: any) =>
+        item.name.toLowerCase().includes(filterValue)
+      );
+    } else {
+      return [];
+    }
   }
 }
